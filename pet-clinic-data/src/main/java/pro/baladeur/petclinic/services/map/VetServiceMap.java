7bcce9d49f76691753ca3a79b7fd89d7cdecf116
Zhaoxiong Chen <czx1997_czx@163.com>
2021-06-21 17:18:46 +0800

    4-62 Implement Map Based Services

---
+++ pet-clinic-data/src/main/java/pro/baladeur/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,39 @@
+package pro.baladeur.petclinic.services.map;
+
+import pro.baladeur.petclinic.model.Vet;
+import pro.baladeur.petclinic.services.VetService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Vet findByLastName(String lastName) {
+        return null;
+    }
+}
