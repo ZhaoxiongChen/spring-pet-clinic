5975de350ea323b120303715ae013b611bb32bfa
Zhaoxiong Chen <czx1997_czx@163.com>
2021-06-21 22:27:44 +0800

    5-80 Load Data on Startup with Java

---
+++ pet-clinic-web/src/main/java/pro/baladeur/petclinic/bootstrap/DataLoader.java
@@ -0,0 +1,59 @@
+package pro.baladeur.petclinic.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import pro.baladeur.petclinic.model.Owner;
+import pro.baladeur.petclinic.model.Vet;
+import pro.baladeur.petclinic.services.OwnerService;
+import pro.baladeur.petclinic.services.VetService;
+import pro.baladeur.petclinic.services.map.OwnerServiceMap;
+import pro.baladeur.petclinic.services.map.VetServiceMap;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+    public DataLoader() {
+        ownerService = new OwnerServiceMap();
+        vetService = new VetServiceMap();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Generation of 2 owners
+        Owner o1 = new Owner();
+        o1.setId(1L);
+        o1.setFirstName("Yamamoto");
+        o1.setLastName("Isoroku");
+
+        ownerService.save(o1);
+
+        Owner o2 = new Owner();
+        o2.setId(2L);
+        o2.setFirstName("Gunther");
+        o2.setLastName("Lutjens");
+
+        ownerService.save(o2);
+
+        System.out.println("Owners loaded");
+
+        // Generation of 2 vets
+        Vet v1 = new Vet();
+        v1.setId(1L);
+        v1.setFirstName("Nikolai");
+        v1.setLastName("Kuznetsov");
+
+        vetService.save(v1);
+
+        Vet v2 = new Vet();
+        v2.setId(2L);
+        v2.setFirstName("William");
+        v2.setLastName("Halsey");
+
+        vetService.save(v2);
+
+        System.out.println("Vets loaded");
+    }
+}
