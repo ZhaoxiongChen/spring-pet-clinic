94e687c45790e0ff9e7424287258dc65ddb3bd1f
Zhaoxiong Chen <czx1997_czx@163.com>
2021-06-21 20:13:31 +0800

    4-66 Create Vet Page and Controller

--- pet-clinic-web/src/main/java/pro/baladeur/petclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/pro/baladeur/petclinic/controllers/IndexController.java
@@ -8,6 +8,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "index", "index.html"})
     public String index() {
+        // Return the name of template to be accessed
         return "index";
     }
 
