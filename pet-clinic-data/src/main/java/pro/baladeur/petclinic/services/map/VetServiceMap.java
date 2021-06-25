8b4ee8e92d41b5a1e35dd44765961a512c882aa2
Zhaoxiong Chen <czx1997_czx@163.com>
2021-06-25 21:07:40 +0800

    7-126 Add Specialities to Vets on Startup

--- pet-clinic-data/src/main/java/pro/baladeur/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/pro/baladeur/petclinic/services/map/VetServiceMap.java
@@ -1,13 +1,21 @@
 package pro.baladeur.petclinic.services.map;
 
 import org.springframework.stereotype.Service;
+import pro.baladeur.petclinic.model.Speciality;
 import pro.baladeur.petclinic.model.Vet;
+import pro.baladeur.petclinic.services.SpecialityService;
 import pro.baladeur.petclinic.services.VetService;
 
 import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialityService specialityService;
+
+    public VetServiceMap(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
 
     @Override
     public Set<Vet> findAll() {
@@ -21,6 +29,20 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+
+        // Defensive coding to avoid error caused by unknown speciality
+        if (object.getSpecialities().size() > 0) {
+            // Check every speciality of the vet
+            object.getSpecialities().forEach(speciality -> {
+                // Find a speciality that is not being persisted
+                if (speciality.getId() == null) {
+                    // Make sure the new speciality is saved and its ID is created
+                    Speciality savedSpeciality = specialityService.save(speciality);
+                    savedSpeciality.setId(speciality.getId());
+                }
+            });
+        }
+
         return super.save(object);
     }
 
--- pet-clinic-web/src/main/java/pro/baladeur/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/pro/baladeur/petclinic/bootstrap/DataLoader.java
@@ -2,11 +2,9 @@ package pro.baladeur.petclinic.bootstrap;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import pro.baladeur.petclinic.model.Owner;
-import pro.baladeur.petclinic.model.Pet;
-import pro.baladeur.petclinic.model.PetType;
-import pro.baladeur.petclinic.model.Vet;
+import pro.baladeur.petclinic.model.*;
 import pro.baladeur.petclinic.services.OwnerService;
+import pro.baladeur.petclinic.services.SpecialityService;
 import pro.baladeur.petclinic.services.VetService;
 import pro.baladeur.petclinic.services.PetTypeService;
 
@@ -18,16 +16,28 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
+    private final SpecialityService specialityService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
+        this.specialityService = specialityService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        int count = petTypeService.findAll().size();
+
+        if (count == 0) {
+            // Load data when the pet type is empty
+            loadData();
+        }
+    }
+
+    private void loadData() {
+        /* Owners */
         // Generate 2 pet types: dog and cat
         PetType dog = new PetType();
         dog.setName("Dog");
@@ -72,16 +82,32 @@ public class DataLoader implements CommandLineRunner {
 
         System.out.println("Owners loaded");
 
+        /* Vets */
+        // Create 3 types of specialities
+        Speciality radiology = new Speciality();
+        radiology.setDescription("Radiology");
+        Speciality savedRadiology = specialityService.save(radiology);
+
+        Speciality surgery = new Speciality();
+        surgery.setDescription("Surgery");
+        Speciality savedSurgery = specialityService.save(surgery);
+
+        Speciality dentistry = new Speciality();
+        dentistry.setDescription("Dentistry");
+        Speciality savedDentistry = specialityService.save(dentistry);
+
         // Generate 2 vets
         Vet v1 = new Vet();
         v1.setFirstName("Nikolai");
         v1.setLastName("Kuznetsov");
+        v1.getSpecialities().add(savedRadiology);
 
         vetService.save(v1);
 
         Vet v2 = new Vet();
         v2.setFirstName("William");
         v2.setLastName("Halsey");
+        v2.getSpecialities().add(savedSurgery);
 
         vetService.save(v2);
 
