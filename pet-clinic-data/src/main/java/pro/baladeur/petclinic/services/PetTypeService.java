1957527e5be685f7de5447bcf00985252465a68b
Zhaoxiong Chen <czx1997_czx@163.com>
2021-06-25 14:28:34 +0800

    7-122 Create Pet Type Map Service

---
+++ pet-clinic-data/src/main/java/pro/baladeur/petclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package pro.baladeur.petclinic.services;
+
+import pro.baladeur.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/pro/baladeur/petclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,34 @@
+package pro.baladeur.petclinic.services.map;
+
+import pro.baladeur.petclinic.model.PetType;
+import pro.baladeur.petclinic.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
